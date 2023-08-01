a02c6bb2f36426254b7aa07541d93749a63ebcda
Student <student@epicodus-4.hsd1.or.comcast.net>
2023-08-01 12:01:10 -0700

    initial commit

---
+++ UniversityRegistrar/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace UniversityRegistrar.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();ß
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
